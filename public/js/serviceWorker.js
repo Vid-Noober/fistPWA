61ca52362ff6d3eec720a2a65b49e31736aa5c7b
Vid-Noober <vidkaka248@.com>
2026-06-26 16:09:43 +0700

    modify-sw-2

--- public/js/serviceWorker.js
+++ public/js/serviceWorker.js
@@ -1,3 +1,5 @@
+var CACHE_NAME = "my-test-pwa-v2";
+
 var assets = [
   "/",
   "/index.html",
@@ -6,17 +8,33 @@ var assets = [
 ];
 
 self.addEventListener("install", function(installEvent) {
-    installEvent.waitUntil(
-        caches.open("my-test-pwa").then(function(cache) {
-            cache.addAll(assets)
+  self.skipWaiting();
+  installEvent.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("activate", function(activateEvent) {
+  activateEvent.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) {
+          return key !== CACHE_NAME;
+        }).map(function(key) {
+          return caches.delete(key);
         })
-    )
-})
+      );
+    })
+  );
+  self.clients.claim();
+});
 
 self.addEventListener("fetch", function(fetchEvent) {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(function(res) {
-            return res || fetch(fetchEvent.request)
-        })
-    )
-})
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(function(res) {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
